9161589dda5c27d4e3ba827a20b3fc037f474bfc
Galonite <astralimprisonment2@gmail.com>
2025-08-08 23:18:15 -0400

    Intitial Commit

--- StatMaker.java
+++ StatMaker.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class StatMaker {
     
+    private static Unit GenUnit;
     public enum DiffPreset{
         EASY,
         MEDIUM,
@@ -15,19 +16,58 @@ public class StatMaker {
         System.out.println("1. Easy\n2. Medium\n3. Hard\4. Boss\nCHOICE: ");
         int choice = scnr.nextInt();
         
-        switch(choice){
-            case 1:
-                break;
-            case 2:
-                break;
-            case 3:
-                break;
-            case 4:
-                break;
-            default:
-                choice = -1;
-                System.out.println("Choice not found, please try again!");
-                break;
+        while(choice > 4 || choice < 0){
+            switch(choice){
+                case 1 -> {
+                    System.out.println("Enter name for the unit: ");
+                    String name = scnr.nextLine();
+                    System.out.println("Generating unit preset...");
+                    GenUnit = new Unit((int) Math.random() * 11, (int) Math.random() * 51 + 50, 
+                    Math.random() * 1, Math.random() * 3, name);
+                }
+                case 2 -> {
+                    System.out.println("Enter name for the unit: ");
+                    String name = scnr.nextLine();
+                    System.out.println("Generating unit preset...");
+                    GenUnit = new Unit((int) Math.random() * 11 + 20, (int) Math.random() * 76 + 125, 
+                    Math.random() * 1 + 1, Math.random() * 2 + 3, name);
+                }
+                case 3 -> {
+                    System.out.println("Enter name for the unit: ");
+                    String name = scnr.nextLine();
+                    System.out.println("Generating unit preset...");
+                    GenUnit = new Unit((int) Math.random() * 11, (int)Math.random() * 51 + 50, 
+                    Math.random() * 1 + 2, Math.random() * 2 + 5, name);
+                }
+                case 4 -> {
+                    System.out.println("Enter name for the unit: ");
+                    String name = scnr.nextLine();
+                    System.out.println("Generating unit preset...");
+                    GenUnit = new Unit((int) Math.random() * 11, (int) Math.random() * 51 + 50, 
+                    Math.random() * 1, Math.random() * 3, name);
+                }
+                default -> {
+                    System.out.println("Choice not found, please try again!");
+                    System.out.println("1. Easy\n2. Medium\n3. Hard\4. Boss\nCHOICE: ");
+                    choice = scnr.nextInt();
+                }
+            }
+
+            String repeatAction = "Y";
+            while(repeatAction.charAt(0) == 'Y'){
+                System.out.println("Generated Unit:\nName: " + GenUnit.getName() + "\nDamage: " + GenUnit.getDamage() + "\nHealth: " + GenUnit.getHealth()
+                + "\nSpeed: " + GenUnit.getSpeed() + "\nRange: " + GenUnit.getRange());
+
+                System.out.println("Would you like to adjust values (Y/N): ");
+                repeatAction = scnr.nextLine().toUpperCase();
+                
+                if(repeatAction.charAt(0) == 'Y'){
+                    int damage = scnr.nextInt();
+                    int health = scnr.nextInt();
+                    double speed = scnr.nextDouble();
+                    double range = scnr.nextDouble();
+                }
+            }
         }
         scnr.close();
 
